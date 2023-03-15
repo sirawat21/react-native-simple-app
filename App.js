8a666bbfcf3ed54e7f4feb2738bc4223eb9aeacb
Brett Sira <sirawat21@gmail.com>
2023-03-15 22:33:25 +1100

    Initial project

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Alert, TextInput } from "react-native";
 
 export default function App() {
+  const [message, setMessage] = useState();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{ message || 'Have no incomming message yet.'}</Text>
+      <TextInput 
+        onChangeText={ (Text) => setMessage(Text) }
+        placeholder="Message something here."/>
+      <Button 
+      title="Learn More" 
+      onPress={() => Alert.alert('Clicked !!')}
+      color="#841584"/>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,9 @@
+Commands:
+```
+npx create-expo-app <PROJECT>
+
+npx expo start
+```
+### References
+- React Native [sorce](https://reactnative.dev/docs)
+- Native Wind [sorce](https://www.nativewind.dev)
