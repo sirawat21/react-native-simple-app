af3061cb90bb5d9b31f24d7275149801517de2bc
Brett Sira <sirawat21@gmail.com>
2023-03-19 22:31:27 +1100

    Added Navigation and Home page

--- screens/home/home.screen.jsx
+++ screens/home/home.screen.jsx
@@ -1,9 +1,27 @@
-import { SafeAreaView, Text } from "react-native";
-import tw from "twrnc";
+import { useLayoutEffect } from "react";
+import { SafeAreaView, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
 const HomeScreen = () => {
+  // Adjusting navigation when layout was loaded; remove nav header
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <SafeAreaView style={tw`flex-1 items-center justify-center`}>
-      <Text style={tw`text-md text-red-600`}>App Homepage</Text>
+    <SafeAreaView className="bg-white flex-1">
+      {/* First Section */}
+      <View className="flex-row px-6 ios:mt-8 android:mt-12 items-center space-x-2">
+        <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
+          <Text className="text-[#00BC99] text-3xl font-semibold">Go</Text>
+        </View>
+        <Text className="text-[#2A2B4B] text-3xl font-semibold">Travel</Text>
+      </View>
+      {/* Second Section */}
+      <View></View>
     </SafeAreaView>
   );
 };
