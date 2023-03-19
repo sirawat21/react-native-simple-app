470a39fbe9a743b2d47ffa576b490a9672f57d8f
Brett Sira <sirawat21@gmail.com>
2023-03-19 18:27:41 +1100

    Created simple home page

--- App.js
+++ App.js
@@ -1,23 +1,17 @@
-import { View } from "react-native";
-import { withExpoSnack, styled } from 'nativewind';
-import { MyButton, MyText } from "./components";
-
-// Creating styled components
-const StyledView = styled(View);
-
-// Event handlering functions
-const firstButtonOnPress = () => alert('It has clicked.');
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, Text, View } from "react-native";
+import tw from "twrnc";
+import { NavigationContainer } from '@react-navigation/native';
+// import { MyButton, MyText } from "./components";
 
 const App = () => {
   return (
-    <StyledView className="flex-1 items-center justify-center bg-slate-400">
-      <MyText className="font-bold text-green-500">TailwindCSS applied style.</MyText>
-      <MyButton 
-        className="rounded-md bg-indigo-600 py-1.5 px-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        onPress={firstButtonOnPress}
-        title="Learn More"/>
-    </StyledView>
+    <SafeAreaView style={tw`flex-1 items-center justify-center`}>
+      <Text style={tw`text-md text-red-600`}>
+        App Homepage
+      </Text>
+    </SafeAreaView>
   );
 }
 
-export default withExpoSnack(App);
+export default App;
