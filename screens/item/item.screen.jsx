dabf633047eb09343d8ed435a95ad111bb127f80
Brett Sira <sirawat21@gmail.com>
2023-03-20 04:30:19 +1100

    Added tumbnial item

--- screens/item/item.screen.jsx
+++ screens/item/item.screen.jsx
@@ -1,9 +1,16 @@
 import { useLayoutEffect } from "react";
-import { View, Text } from "react-native";
-import React from "react";
+import { FontAwesome5 } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
-const ItemScreen = () => {
+const ItemScreen = ({ route }) => {
   // Adjusting navigation when layout was loaded; remove nav header
   const navigation = useNavigation();
   useLayoutEffect(() => {
@@ -12,10 +19,36 @@ const ItemScreen = () => {
     });
   }, []);
 
+  // Get passed params
+  const data = route?.params?.param;
+
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-white relative">
       <Text>item screen</Text>
-    </View>
+      <ScrollView className="flex-1 px-4 py-6">
+        <View className="relative bg-white shadow-lg">
+          <Image
+            source={{
+              uri: data?.photo?.images?.large?.url
+                ? data?.photo?.images?.large?.url
+                : "https://cdn.vectorstock.com/i/1000x1000/82/99/no-image-available-like-missing-picture-vector-43938299.webp",
+            }}
+            className="w-full h-72 object-cover rounded-2xl"
+          />
+          <View className="absolute flex-row inset-x-0 top-5 justify-between px-6">
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Discover")}
+              className="w-10 h-10 rounded-md items-center justify-center bg-white"
+            >
+              <FontAwesome5 name="chevron-lef" size={24} color="#06B@BE" />
+            </TouchableOpacity>
+            <TouchableOpacity className="w-10 h-10 rounded-md items-center justify-center bg-[#06B2BE]">
+              <FontAwesome5 name="heartbeat" size={24} color="#FFFF" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
