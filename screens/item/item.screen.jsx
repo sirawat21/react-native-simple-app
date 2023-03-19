b00ef01beb05b5644e537931ccbd32996c8748e4
Brett Sira <sirawat21@gmail.com>
2023-03-20 04:52:08 +1100

    Added rating section

--- screens/item/item.screen.jsx
+++ screens/item/item.screen.jsx
@@ -1,5 +1,5 @@
 import { useLayoutEffect } from "react";
-import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome5, FontAwesome } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import {
   View,
@@ -23,8 +23,8 @@ const ItemScreen = ({ route }) => {
   const data = route?.params?.param;
 
   return (
-    <SafeAreaView className="flex-1 bg-white relative">
-      <Text>item screen</Text>
+    <SafeAreaView className="flex-1 bg-white relative android:mt-6 ios:mt-2">
+      {/* Header */}
       <ScrollView className="flex-1 px-4 py-6">
         <View className="relative bg-white shadow-lg">
           <Image
@@ -40,12 +40,57 @@ const ItemScreen = ({ route }) => {
               onPress={() => navigation.navigate("Discover")}
               className="w-10 h-10 rounded-md items-center justify-center bg-white"
             >
-              <FontAwesome5 name="chevron-lef" size={24} color="#06B@BE" />
+              <FontAwesome
+                name="arrow-circle-o-left"
+                size={24}
+                color="#06B@BE"
+              />
             </TouchableOpacity>
             <TouchableOpacity className="w-10 h-10 rounded-md items-center justify-center bg-[#06B2BE]">
               <FontAwesome5 name="heartbeat" size={24} color="#FFFF" />
             </TouchableOpacity>
           </View>
+          <View className="absolute flex-row inset-x-0 bottom-5 justify-between px-6">
+            <View className="flex-row space-x-2 items-center">
+              <Text className="text-[12px] font-bold text-gray-100">
+                {data?.price_level}
+              </Text>
+              <Text className="text-[32px] font-bold text-gray-100">
+                {data?.price}
+              </Text>
+            </View>
+            <View className="px-2 py-1 rounded-md bg-teal-100">
+              <Text>{data?.open_now_text}</Text>
+            </View>
+          </View>
+        </View>
+
+        {/* Body  */}
+        {/* Detail Location */}
+        <View className="mt-6">
+          <Text className="text-[#428288] text-[24px] font-bold">
+            {data?.name}
+          </Text>
+          <View className="flex-row items-center space-x-2 mt-2">
+            <FontAwesome name="map-marker" size={25} color="#8C9EA6" />
+            <Text className="text-[#8C9EA6] text-[20px] font-bold">
+              {data?.location_string}
+            </Text>
+          </View>
+        </View>
+        {/* Rating */}
+        <View className="mt-4 flex-row items-center justify-between">
+          {data?.rating && (
+            <View className="flex-row items-center space-x-2">
+              <View className="w-12 h-12 rounded-2xl bg-red-100 items-center justify-center shadow-md">
+                <FontAwesome name="star" size={24} color="#D58574" />
+              </View>
+              <View>
+                <Text className="text-[#515151]">{data?.rating}</Text>
+                <Text className="text-[#515151]">Ratings</Text>
+              </View>
+            </View>
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
